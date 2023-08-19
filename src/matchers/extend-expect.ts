6a97b00fa9c63901c3d1f347d2dd00f61ce7332f
Jan Jaworski <jaworek3211@gmail.com>
2023-08-19 21:19:02 +0200

    add tests for toHaveDisplayValue()

--- src/matchers/extend-expect.ts
+++ src/matchers/extend-expect.ts
@@ -1,7 +1,9 @@
 /// <reference path="./extend-expect.d.ts" />
 
 import { toBeOnTheScreen } from './to-be-on-the-screen';
+import { toHaveDisplayValue } from './to-have-display-value';
 
 expect.extend({
   toBeOnTheScreen,
+  toHaveDisplayValue,
 });
